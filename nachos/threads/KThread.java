f24751d299d103d31aa470734f09fb375c9af6e9
sai sundar raghavan <saisundaraghavan@gmail.com>
2014-01-26 18:41:29 -0800

    for proj0
    sooi's proj 0

--- nachos/threads/KThread.java
+++ nachos/threads/KThread.java
@@ -412,9 +412,10 @@ public class KThread {
 	 */
 	public static void selfTest() {
 		Lib.debug(dbgThread, "Enter KThread.selfTest");
-
-		new KThread(new PingTest(1)).setName("forked thread").fork();
 		new PingTest(0).run();
+		new KThread(new PingTest(1)).setName("forked thread").fork();
+		new PingTest(2).run();
+
 	}
 
 	private static final char dbgThread = 't';
