db865c14280617ea93638d60265382fc58358aa5
sajosg <sajog@uci.edu>
2014-03-07 17:32:27 -0800

    Make safe check to handle invalid user program passed as input

--- nachos/userprog/UserKernel.java
+++ nachos/userprog/UserKernel.java
@@ -108,7 +108,10 @@ public class UserKernel extends ThreadedKernel {
 
 		String shellProgram = Machine.getShellProgramName();
 		//Lib.assertTrue(process.execute(shellProgram, new String[] {}));
-		Lib.assertTrue(process.execute(shellProgram, Machine.getShellProgramArgs()));
+		if (!process.execute(shellProgram, Machine.getShellProgramArgs())){
+			System.out.println("Process terminating due to invalid shell program " + shellProgram);
+			terminate();
+		}
 
 		KThread.currentThread().finish();
 	}
